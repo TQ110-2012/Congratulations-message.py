628a444d8c3b483d3e12302468f02faeabefc984
TQ110-2012 <tanizqasim@gmail.com>
2025-09-19 22:25:38 +0500

    Add files via upload

---
+++ Congratulations msg.py
@@ -0,0 +1,7 @@
+print("Tell me the answer to this question.")
+question= int(input("What is the sum of 265 and 54?"))
+answer= int(256+54)
+if question==310 :
+    print("Congratulations! you are right.")
+else:
+    print("No the answer is ",answer)
